b9cc75ccaa63f06cf2daba8598d81e143981708e
isadmentes <isadmentes@gmail.com>
2025-10-02 21:43:44 -0300

    Implementado um grid responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 // apagou-se tudo e copiou-se o que estava na lousa //
 const App = () => {
   return (
-    <main className="min-h-dvh grid place-items-center bg-blue-900">
+    <main className="min-h-dvh place-items-center bg-blue-900">
       <h1 className="text-3xl font-bold text-slate-800">
         Hello Tailwind + React!
       </h1>
+
+      <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="rounded-2xl border p-4 bg-white shadow-sm">
+        Card A
+      </div>
+      <div className="rounded-2xl border p-4 bg-white shadow-sm">
+        Card B
+      </div>
+      <div className="rounded-2xl border p-4 bg-white shadow-sm">
+        Card C
+      </div>
+    </div>
     </main>
   )
 }
