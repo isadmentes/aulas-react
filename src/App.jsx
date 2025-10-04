1bf2a7e2c8a1364659fdfa3fecc6934aa379d968
isadmentes <isadmentes@gmail.com>
2025-10-03 21:46:34 -0300

    Implementado entrada controlada com estado

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,24 @@ import { Hello } from "./componentes/Hello"
 // apagou-se tudo e copiou-se o que estava na lousa //
 const App = () => {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   const inc = () => setCount(c => c + 1)
 
+  const handleSubmit = event => {
+    event.preventDefault() 
+
+    console.log({ name,email, password })
+    
+    setName("")
+    setEmail("")
+    setPassword("")
+  }
+
   return (
-    <main className="min-h-dvh place-items-center bg-blue-900">
+    <main className="place-items-center bg-blue-900">
       <h1 className="text-3xl font-bold text-slate-800">
         Hello Tailwind + React!
       </h1>
@@ -38,6 +51,60 @@ const App = () => {
       >
         contador {count}
       </button>
+
+      <form onSubmit={handleSubmit}>
+        <fieldset className="flex flex-col space-y-2 mb-2">
+          <label htmlFor="name" className="font-medium">Digite seu nome </label>
+
+        <input
+          id="name"
+          name="name"
+          placeholder="Nome completo"
+          className="py-2 px-4 border rounded-2xl w-full"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+
+        </fieldset>
+
+        <fieldset className="flex flex-col space-y-2 mb-2">
+          <label htmlFor="email" className="font-medium">Digite seu email </label>
+
+        <input
+          id="email"
+          name="email"
+          placeholder="email"
+          className="py-2 px-4 border rounded-2xl w-full"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+       
+        </fieldset>
+
+        <fieldset className="flex flex-col space-y-2 mb-2">
+          <label htmlFor="password" className="font-medium">Digite sua senha </label>
+
+        <input
+          id="password"
+          name="password"
+          placeholder="password"
+          type="password"
+          className="py-2 px-4 border rounded-2xl w-full"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+
+        </fieldset> 
+
+        <button
+        type="submit"
+      className="inline-block px-4 py-2 rounded-1g border hover:bg-amber-700 transition-all cursor-pointer focus:ring-2 focus:ring-blue-500"
+      >
+        Enviar
+      </button>
+      </form>
+
+
     </main>
   )
 }
